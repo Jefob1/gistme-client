05b3630f6893cabc731fcbc55e02f2340798a3f6
Jefob1 <89314101+Jefob1@users.noreply.github.com>
2023-10-25 23:55:48 +0100

    slight modifications

--- src/socketContext.js
+++ src/socketContext.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 const SocketContext = React.createContext()
 
 const SocketProvider = ({ children }) => {
-    const ENDPOINT = 'https://socket-chat-ak.herokuapp.com/';
+    const ENDPOINT = 'https://gistme-server.onrender.com/';
     const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] })
     return (
         <SocketContext.Provider value={socket}>
